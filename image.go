db5c9dc05ee2d2fa834fc5939e1086afed5e368f
Alex Nichol <unixpickle@gmail.com>
2016-10-12 15:56:30 -0400

    implemented cost function & outlined image routine

---
+++ image.go
@@ -0,0 +1,13 @@
+package autorot
+
+import "image"
+
+// Rotate rotates an image around its center and returns
+// the largest centered square cropping that does not go
+// out of the rotated image's bounds.
+//
+// The angle is specified in clockwise radians.
+func Rotate(img image.Image, angle float64) image.Image {
+	// TODO: this.
+	panic("not yet implemented")
+}
