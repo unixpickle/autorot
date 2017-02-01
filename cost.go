8a666e1548c8d04c51ecd98ed74f1e01affe2593
Alex Nichol <unixpickle@gmail.com>
2017-01-31 20:50:50 -0500

    pool cost inputs to prevent double-backprop

--- cost.go
+++ cost.go
@@ -8,12 +8,17 @@ type Cost struct{}
 
 // Cost computes the cost.
 func (c Cost) Cost(a, b anydiff.Res, n int) anydiff.Res {
+	dotProducts := anydiff.Pool(a, func(a anydiff.Res) anydiff.Res {
+		return anydiff.Pool(b, func(b anydiff.Res) anydiff.Res {
+			return anydiff.Add(
+				anydiff.Mul(anydiff.Sin(a), anydiff.Sin(b)),
+				anydiff.Mul(anydiff.Cos(a), anydiff.Cos(b)),
+			)
+		})
+	})
+
 	// Utilize the fact that the dot product between two unit
 	// vectors is < 1 when the vectors are not equal.
-	dotProducts := anydiff.Add(
-		anydiff.Mul(anydiff.Sin(a), anydiff.Sin(b)),
-		anydiff.Mul(anydiff.Cos(a), anydiff.Cos(b)),
-	)
 	differences := anydiff.Complement(dotProducts)
 	mat := &anydiff.Matrix{
 		Data: differences,
