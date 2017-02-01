6ac27be1251a422be53fbb4af9e846a6035377dd
Alex Nichol <unixpickle@gmail.com>
2017-02-01 10:27:30 -0500

    import anyconv package for deserializing

--- network.go
+++ network.go
@@ -8,6 +8,8 @@ import (
 	"github.com/unixpickle/anyvec"
 	"github.com/unixpickle/anyvec/anyvec32"
 	"github.com/unixpickle/serializer"
+
+	_ "github.com/unixpickle/anynet/anyconv"
 )
 
 func init() {
